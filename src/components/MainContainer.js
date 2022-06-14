f30267539522c6bf6ea9f228b1803a0c2435f01c
majaliju <majalijunyc@gmail.com>
2022-06-13 22:52:33 -0400

    updated, better appraoch & cleaner

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -7,12 +7,26 @@ function MainContainer() {
 
   const [stocks, setStocks] = useState([])
   const [myStocks, setMyStocks] = useState([])
+
+  // sorting results by either alph or price
+  const [sortBy, setSortBy] = useState('')
   
   useEffect(() => {
     fetch('http://localhost:3001/stocks')
     .then(response => response.json())
     .then(data => setStocks(data))
   }, [])
+
+  useEffect(() => {
+    if(sortBy === 'Alphabetically'){
+      const sortedStocks = sortByName()
+      setStocks(sortedStocks)
+    }
+    else{
+      const sortedStocks = sortByPrice()
+      setStocks(sortedStocks)
+    }
+  }, [sortBy])
 
   const buyStock = (stock) => {
     if(!myStocks.includes(stock)){
@@ -29,9 +43,36 @@ function MainContainer() {
     setMyStocks(updatedMyStocks)
   }
 
+  const sortStocks = (e) => {
+    // e.target.value for the event listener
+    setSortBy(e.target.value)
+  }
+  
+  const sortByName = () => {
+    return [...stocks].sort(function(a, b) {
+      const nameA = a.name.toUpperCase(); // ignore upper and lowercase
+      const nameB = b.name.toUpperCase(); // ignore upper and lowercase
+      if (nameA < nameB) {
+        return -1;
+      }
+      if (nameA > nameB) {
+        return 1;
+      }
+    
+      return 0;
+    });
+  }
+
+  const sortByPrice = () => {
+    return [...stocks].sort(function (a, b) {
+      return a.price - b.price;
+    });
+    
+  }
+
   return (
     <div>
-      <SearchBar />
+      <SearchBar sortStocks = {sortStocks}/>
       <div className="row">
         <div className="col-8">
           <StockContainer 
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,6 +1,7 @@
 import React from "react";
 
-function SearchBar() {
+function SearchBar({sortStocks}) {
+
   return (
     <div>
       <strong>Sort by:</strong>
@@ -10,7 +11,7 @@ function SearchBar() {
           value="Alphabetically"
           name="sort"
           checked={null}
-          onChange={null}
+          onChange={sortStocks}
         />
         Alphabetically
       </label>
@@ -20,7 +21,7 @@ function SearchBar() {
           value="Price"
           name="sort"
           checked={null}
-          onChange={null}
+          onChange={sortStocks}
         />
         Price
       </label>
